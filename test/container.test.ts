9d5533600dd81c98f5f924aa7f77b33ccdcd7604
Pierre Leroux <pierre@leroux.tech>
2020-11-27 16:11:07 +0100

    add immer container test

--- test/container.test.ts
+++ test/container.test.ts
@@ -1,7 +1,7 @@
 import * as assert from 'assert';
-import {Container} from '../src/Container';
+import {Container, ContainerWithImmer} from '../src/Container';
 
-const test = () => {
+const test1 = () => {
   interface ITest {
     hello?: string;
     world?: string;
@@ -65,4 +65,169 @@ const test = () => {
   }
 };
 
-test();
+test1();
+
+
+const test2 = () => {
+  interface ITest {
+    hello?: string;
+    world?: string;
+  }
+  const test: ITest = {};
+
+  {
+    const c = new ContainerWithImmer<ITest, keyof ITest>(test);
+    {
+      c.set('hello', 'world');
+      c.set('world', 'hello');
+
+      const v1 = c.get('hello');
+      const v2 = c.get('world');
+
+      assert.deepStrictEqual(v1, 'world');
+      assert.deepStrictEqual(v2, 'hello');
+
+      c.delete('world');
+
+      const v3 = c.get('hello');
+      const v4 = c.get('world');
+
+      assert.deepStrictEqual(v3, 'world');
+      assert.deepStrictEqual(v4, undefined);
+
+      c.delete('hello');
+
+      const v5 = c.get('hello');
+      const v6 = c.get('world');
+
+      assert.deepStrictEqual(v5, undefined);
+      assert.deepStrictEqual(v6, undefined);
+    }
+    {
+      c.set('hello', 'world');
+      c.set('world', 'hello');
+
+      const v1 = c.get('hello');
+      const v2 = c.get('world');
+
+      assert.deepStrictEqual(v1, 'world');
+      assert.deepStrictEqual(v2, 'hello');
+
+      c.delete('world');
+
+      const v3 = c.get('hello');
+      const v4 = c.get('world');
+
+      assert.deepStrictEqual(v3, 'world');
+      assert.deepStrictEqual(v4, undefined);
+
+      c.delete('hello');
+
+      const v5 = c.get('hello');
+      const v6 = c.get('world');
+
+      assert.deepStrictEqual(v5, undefined);
+      assert.deepStrictEqual(v6, undefined);
+    }
+  }
+};
+
+test2();
+
+const test3 = () => {
+  interface ITest {
+    hello?: { test: string };
+    world?: { test: string };
+  }
+  const test: ITest = {};
+
+  {
+    const c = new ContainerWithImmer<ITest, keyof ITest>(test);
+    {
+      c.set('hello', { test: "world"});
+      c.set('world', { test: 'hello'});
+
+      const v1 = c.getDraft('hello');
+      const v2 = c.getDraft('world');
+
+      if (v1) {
+        v1.test = "immer";
+      }
+      if (v2) {
+        v2.test = "remmi";
+      }
+
+      assert.deepStrictEqual(v1?.test, 'immer');
+      assert.deepStrictEqual(v2?.test, 'remmi');
+
+      c.set('hello', v1);
+      c.set('world', v2);
+
+      const v31 = c.get('hello')?.test;
+      const v41 = c.get('world')?.test;
+
+      assert.deepStrictEqual(v31, 'immer');
+      assert.deepStrictEqual(v41, 'remmi');
+      
+      c.delete('world');
+
+      const v3 = c.get('hello')?.test;
+      const v4 = c.get('world')?.test;
+
+      assert.deepStrictEqual(v3, 'immer');
+      assert.deepStrictEqual(v4, undefined);
+
+      c.delete('hello');
+
+      const v5 = c.get('hello')?.test;
+      const v6 = c.get('world')?.test;
+
+      assert.deepStrictEqual(v5, undefined);
+      assert.deepStrictEqual(v6, undefined);
+    }
+    {
+      c.set('hello', { test: "world"});
+      c.set('world', { test: 'hello'});
+
+      const v1 = c.getDraft('hello');
+      const v2 = c.getDraft('world');
+
+      if (v1) {
+        v1.test = "immer";
+      }
+      if (v2) {
+        v2.test = "remmi";
+      }
+
+      assert.deepStrictEqual(v1?.test, 'immer');
+      assert.deepStrictEqual(v2?.test, 'remmi');
+
+      c.set('hello', v1);
+      c.set('world', v2);
+
+      const v31 = c.get('hello')?.test;
+      const v41 = c.get('world')?.test;
+
+      assert.deepStrictEqual(v31, 'immer');
+      assert.deepStrictEqual(v41, 'remmi');
+      
+      c.delete('world');
+
+      const v3 = c.get('hello')?.test;
+      const v4 = c.get('world')?.test;
+
+      assert.deepStrictEqual(v3, 'immer');
+      assert.deepStrictEqual(v4, undefined);
+
+      c.delete('hello');
+
+      const v5 = c.get('hello')?.test;
+      const v6 = c.get('world')?.test;
+
+      assert.deepStrictEqual(v5, undefined);
+      assert.deepStrictEqual(v6, undefined);
+    }
+  }
+};
+
+test3();
