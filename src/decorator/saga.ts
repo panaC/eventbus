91d238d51cf45292842f1d21350c55c2f2210b6b
Pierre Leroux <pierre@leroux.tech>
2020-11-29 18:20:11 +0100

    add redux-saga like a decorator and It's works !

---
+++ src/decorator/saga.ts
@@ -0,0 +1,48 @@
+import {TContainer} from '../Container';
+import createSagaMiddleware, { SagaMiddleware } from 'redux-saga'
+import { TMaybePromise } from '../type/fn.type';
+import { Dispatch } from '../Dispatch';
+
+export interface ISaga {
+    runSaga: SagaMiddleware["run"];
+}
+
+export const saga = <
+  TClass extends {
+    new (...a: any[]): Dispatch<T, U, V>;
+  },
+  T extends TContainer<V>,
+  U extends TContainer<V>,
+  V extends string
+>(
+  constructor: TClass
+) => {
+    return class extends constructor {
+        constructor(...a: any[]) {
+            super(...a);
+        }
+
+        _sagaMiddleware = createSagaMiddleware();
+
+        _sagaMiddlewareDispatch = this._sagaMiddleware({
+            dispatch: (action) => {
+                const { type, value: actionValue } = action;
+                this.dispatch(type as V, actionValue);
+                return action;
+            },
+            getState: () => this.get,
+        });
+
+        runSaga = this._sagaMiddleware.run;
+
+        dispatch<TK extends V>(key: TK, value: TMaybePromise<T[TK]>) {
+
+            this._sagaMiddlewareDispatch((action) => {
+                super.dispatch(key, value);
+                return action;
+            })({ type: key, value });
+
+            return this;
+        };
+    }
+};
---
+++ test/saga.test.ts
@@ -0,0 +1,54 @@
+import {Dispatch} from '../src/Dispatch';
+import {
+  eventEmitter,
+  IEventEmitter,
+  TContainerWithEventEmitter,
+} from '../src/decorator/eventEmitter';
+import * as assert from 'assert';
+import { ISaga, saga } from '../src/decorator/saga';
+import { take } from 'redux-saga/effects';
+
+const test = () => {
+  {
+    interface ITest2 {
+      hello?: string;
+      world?: number;
+    }
+
+    interface MyColonne
+      extends Dispatch<ITest2, TEventEmitter, keyof ITest2>,
+        IEventEmitter<ITest2, keyof ITest2>,
+        ISaga {}
+
+    type TEventEmitter = TContainerWithEventEmitter<keyof ITest2>;
+
+    @saga
+    @eventEmitter
+    class MyColonne extends Dispatch<ITest2, TEventEmitter, keyof ITest2> {}
+    const ev = new MyColonne({});
+
+    const fn = (bus: any, data: any) => {
+      console.log('HELLO', data);
+      assert.deepStrictEqual('world', data);
+      assert.deepStrictEqual(bus, ev);
+    };
+
+    ev.runSaga(function*() {
+      console.log('HELLO WORLD IT\'s ME SAGA');
+
+      while (1) {
+        yield take("hello");
+        console.log("hello TAKED in SAGA");
+
+      }
+      
+    });
+
+    ev.subscribe('hello', fn)
+      .dispatch('hello', 'world')
+      .unsubscribe('hello', fn)
+      .dispatch('hello', 'should not subscribe');
+  }
+};
+
+test();
